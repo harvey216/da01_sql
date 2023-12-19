c696541a0a957a3af482757cac7576b82b62676c
harvey216 <154249637+harvey216@users.noreply.github.com>
2023-12-19 22:53:10 +0700

    Update day3_baitap.sql

--- day3_baitap.sql
+++ day3_baitap.sql
@@ -22,3 +22,36 @@ where city like '%A' or city like '%E' or city like '%O' or city like 'II' or ci
 --bài 6
 SELECT DISTINCT CITY FROM STATION WHERE upper(SUBSTR(CITY,1,1)) NOT IN ('A','E','I','O','U') AND lower(SUBSTR(CITY,1,1)) NOT IN
 ('a','e','i','o','u');     
+
+--bài 7
+select name from employee
+order by name
+
+  --bài 8
+select name from employee 
+where salary > 2000 and months <10
+
+--bài 9
+select product_id from products
+where low_fats ='Y' and recyclable ='Y'
+
+--bài 10
+select name, population, area from world
+where area >= 3000000 or population >= 25000000
+
+--bài 11
+select distinct author_id as id from views
+where author_id = viewer_id
+
+--bài 13
+SELECT part, assembly_step FROM parts_assembly
+WHERE finish_date is NULL;
+
+--bài 14
+select * from lyft_drivers
+where yearly_salary <= 30 or yearly_salary >= 70
+
+--bài 15
+select advertising_channel from uber_advertising
+where money_spent > 100 and year = 2019
+
